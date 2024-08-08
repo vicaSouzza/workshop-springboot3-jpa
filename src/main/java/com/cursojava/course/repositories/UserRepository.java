f29c6180ead275622c7fd89a8770c13b8e2b8382
vicaSouzza <vitoriasouzza.vrs@gmail.com>
2024-08-08 17:40:17 -0300

    Service layer, component registration

--- src/main/java/com/cursojava/course/repositories/UserRepository.java
+++ src/main/java/com/cursojava/course/repositories/UserRepository.java
@@ -3,5 +3,6 @@ package com.cursojava.course.repositories;
 import com.cursojava.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 }
---
+++ src/main/java/com/cursojava/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.cursojava.course.services;
+
+import com.cursojava.course.entities.User;
+import com.cursojava.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+       Optional<User> obj = repository.findById(id);
+       return obj.get();
+    }
+}
