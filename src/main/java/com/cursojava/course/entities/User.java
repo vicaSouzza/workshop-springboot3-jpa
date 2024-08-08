09b158ec164744c0640ee4a82d3e1c8f645eb8e1
vicaSouzza <vitoriasouzza.vrs@gmail.com>
2024-08-08 18:23:59 -0300

    Order, Instant, ISO 8601 PART 1

--- src/main/java/com/cursojava/course/entities/User.java
+++ src/main/java/com/cursojava/course/entities/User.java
@@ -3,6 +3,8 @@ package com.cursojava.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -20,6 +22,10 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private final List<Order> orders = new ArrayList<>();
+
 
 
 
@@ -75,6 +81,10 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -87,4 +97,6 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+
 }
