cbe115e282c49befd85a7b0dcc6b51f26ed67a4f
vicaSouzza <vitoriasouzza.vrs@gmail.com>
2024-08-15 18:32:50 -0300

    Payment, one-to-one association

---
+++ src/main/java/com/cursojava/course/entities/Payment.java
@@ -0,0 +1,73 @@
+package com.cursojava.course.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+
+    @OneToOne
+    @MapsId
+    private Order order;
+
+    public Payment() {
+
+    }
+
+    public Payment(Long id, Instant moment, Order order) {
+        this.id = id;
+        this.moment = moment;
+        this.order = order;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Payment payment = (Payment) o;
+
+        if (!id.equals(payment.id)) return false;
+        if (!moment.equals(payment.moment)) return false;
+        return order.equals(payment.order);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + moment.hashCode();
+        result = 31 * result + order.hashCode();
+        return result;
+    }
+}
